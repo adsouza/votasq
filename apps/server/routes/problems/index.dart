839953d53b539303df9b6456540776a8a216c79b
Antonio D'souza <275832+adsouza@users.noreply.github.com>
2026-03-14 21:04:39 -0400

    Support listing all problems.

--- apps/server/routes/problems/index.dart
+++ apps/server/routes/problems/index.dart
@@ -6,9 +6,22 @@ import 'package:shared/shared.dart';
 
 Future<Response> onRequest(RequestContext context) async {
   return switch (context.request.method) {
+    HttpMethod.get => _get(context),
     HttpMethod.post => _post(context),
     _ => Future.value(Response(statusCode: 405)),
   };
+}
+
+Future<Response> _get(RequestContext context) async {
+  final db = await context.read<Future<Db>>();
+  try {
+    final problems = await db.getProblems();
+    return Response.json(
+      body: {'data': problems.map((p) => p.toJson()).toList()},
+    );
+  } on Exception {
+    return Response(statusCode: 500);
+  }
 }
 
 Future<Response> _post(RequestContext context) async {
