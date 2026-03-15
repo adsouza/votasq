45c8b0cb0da63bf9b7821e29e8abd0af7dbb14e3
Antonio D'souza <275832+adsouza@users.noreply.github.com>
2026-03-14 21:34:40 -0400

    Make Flutter client display problem list in UI.

---
+++ apps/client/lib/problems/cubit/problems_state.dart
@@ -0,0 +1,30 @@
+import 'package:shared/shared.dart';
+
+enum ProblemsStatus { initial, loading, success, failure }
+
+class ProblemsState {
+  const ProblemsState({
+    this.status = ProblemsStatus.initial,
+    this.problems = const [],
+    this.nextPageToken,
+  });
+
+  final ProblemsStatus status;
+  final List<Problem> problems;
+  final String? nextPageToken;
+
+  bool get hasMore => nextPageToken != null;
+
+  ProblemsState copyWith({
+    ProblemsStatus? status,
+    List<Problem>? problems,
+    String? Function()? nextPageToken,
+  }) {
+    return ProblemsState(
+      status: status ?? this.status,
+      problems: problems ?? this.problems,
+      nextPageToken:
+          nextPageToken != null ? nextPageToken() : this.nextPageToken,
+    );
+  }
+}
---
+++ apps/client/lib/problems/problems.dart
@@ -0,0 +1,3 @@
+export 'cubit/problems_cubit.dart';
+export 'cubit/problems_state.dart';
+export 'view/problems_page.dart';
