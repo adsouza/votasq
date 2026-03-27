eda88f7581dfbc35e4fb25140c5b9819943259d6
Antonio D'souza <275832+adsouza@users.noreply.github.com>
2026-03-27 07:36:04 -0400

    Fix broken tests to use SharedPreferencesWithCache.

--- apps/client/lib/auto_translate/cubit/auto_translate_cubit.dart
+++ apps/client/lib/auto_translate/cubit/auto_translate_cubit.dart
@@ -4,19 +4,29 @@ import 'package:bloc/bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AutoTranslateCubit extends Cubit<bool> {
-  AutoTranslateCubit({bool initial = false}) : super(initial) {
+  AutoTranslateCubit({
+    bool initial = false,
+    SharedPreferencesWithCache? prefsForTesting,
+  }) : super(initial) {
+    _prefsForTesting = prefsForTesting;
     unawaited(_initialize());
   }
 
   static SharedPreferencesWithCache? _prefs;
+  static SharedPreferencesWithCache? _prefsForTesting;
   static const _prefsKey = 'auto_translate';
 
   Future<void> _initialize() async {
-    _prefs = await SharedPreferencesWithCache.create(
-      cacheOptions: const SharedPreferencesWithCacheOptions(
-        allowList: {_prefsKey},
-      ),
-    );
+    // Use injected instance for testing, otherwise create real instance
+    if (_prefsForTesting != null) {
+      _prefs = _prefsForTesting;
+    } else {
+      _prefs = await SharedPreferencesWithCache.create(
+        cacheOptions: const SharedPreferencesWithCacheOptions(
+          allowList: {_prefsKey},
+        ),
+      );
+    }
     final persisted = _prefs?.getBool(_prefsKey);
     if (persisted != null) emit(persisted);
   }
